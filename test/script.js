3149818b3a7bf2ca4bca38839d2a652197b03a22
MiMi T <truttling.mariah@gmail.com>
2021-01-16 10:09:08 -0500

    artist search

--- test/script.js
+++ test/script.js
@@ -1,23 +1,28 @@
+ function artistInfo () {
 
-$("#searchBtn").on("click", function(event) {
+    $("#searchBtn").on("click", function(event) {
 
-    event.preventDefault ();
+
 
     var getArtist= $("#artSearch").val();
     console.log(getArtist)
+    var format= "jsnop";
+    var callBack= "callback";
+    var apikey= "7f6c68b406143881580235194e8517a0";
+    
+    var queryURL="api.musixmatch.com/ws/1.1/artist.search?format="+format +"&callback="+callBack+"&q_artist="+getArtist+"&apikey="+apikey;
     
 
     $.ajax( {
-        data: {
-            format: ""
+        url: queryURL,
+        method: "GET", })
 
-        },
-        url:"api.musixmatch.com/ws/1.1/artist.search"
+       .then(function(response){
+        
+            console.log(response)
+        });
 
-    })
-
+    
 })
-
-
-
-
+ }
+ 
